ffafb488068eb862b4f4057657442e8de43bf316
Geza-Tamas Kovacs <kovacs.geza.tamas@gmail.com>
2020-12-12 16:28:03 +0200

    Lab 11 Personal Agenda

---
+++ kovacs_geza_tamas_lab11/PersonalAgenda/PersonalAgenda/Models/Agenda.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+
+namespace PersonalAgenda.Models
+{
+    public class Agenda
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
